0fa4694d9ad256610ab6246325dd78809344e1fb
Greg Weber <greg@grubtop.(none)>
2008-03-09 10:08:02 -0500

    got rid of module importing madness

--- lib/methodchain/not_included.rb
+++ lib/methodchain/not_included.rb
@@ -1,6 +1,35 @@
-require File.dirname(__FILE__) + '/methodchain'
+# :main: README
+module MethodChain
+  def self_eval &block
+    case block.arity
+    # ruby bug for -1
+    when 0, -1 then instance_eval(&block)
+    when 1     then yield(self)
+    else            raise ArgumentError, "too many arguments required by block"
+    end
+  end
 
-module MethodChain # :nodoc:
-  include Tap
-  include ThenElse
+  def then arg=nil, &block
+    if self
+      block_given? ? (self_eval &block) : (arg || (fail \
+        ArgumentError, "#then must be called with an argument or a block"))
+    else
+      self
+    end
+  end
+
+  def else arg=nil, &block
+    if self
+      self
+    else
+      block_given? ? (self_eval &block) : (arg || (fail \
+        ArgumentError, "#else must be called with an argument or a bloc"))
+    end
+  end
+
+  def tap meth=nil, &block
+    self.send meth if meth
+    self_eval &block if block_given?
+    self
+  end
 end
--- lib/methodchain/then_else.rb
+++
@@ -1,6 +0,0 @@
-require File.dirname(__FILE__) + '/methodchain'
-
-module MethodChain # :nodoc:
-  include ThenElse end
-class Object # :nodoc:
-  include MethodChain end
